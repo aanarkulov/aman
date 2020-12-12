520afeb0ca1fd7a6e0755efaf0878e4f6b194545
aanarkulov <a.anarkuloff@gmail.com>
2020-12-12 16:33:43 +0600

    Добавил 3-е задание

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+const getDividers = (n) => {
+  const arr = [];
+  for (let i = 1; i <= n; i += 1) {
+    if (n % i === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
+
 function App() {
+  const [number, setNumber] = React.useState('');
+  const [result, setResult] = React.useState(null);
+
+  const getResult = (n) => {
+    const dividersN = getDividers(n)
+    const nod = [];
+    for (let i = 1; i < n; i += 1) {
+      const nodInner = getDividers(i).filter((item) => dividersN.includes(item));
+      if (nodInner.length === 1) {
+        nod.push(i);
+      }
+    }
+    setResult(nod.join(' '));
+  };
+
+  const onChange = ({ target: { value }}) => {
+    if (value) {
+      setNumber(Math.abs(value))
+    } else {
+      setNumber(value);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Введите натуральное число N</p>
+      <input type="number" onChange={onChange} value={number} />
+      <button onClick={() => getResult(number)}>Получить все натуральные числа меньше N и взаимно простые с ним</button>
+      {result && <span>{result}</span>}
     </div>
   );
 }
